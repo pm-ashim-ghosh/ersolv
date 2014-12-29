755edd84a9d3c57dfe3bdd15395b45e7fbca1c2f
Ashim Ghosh <ashim.ghosh@pubmatic.com>
2014-12-29 15:29:21 +0530

    JSON encoding/decoding, create log, README.
    - JSON encoding/decoding for creating and fetching logs over API.
    - Create "code log" in database; API to create "code log".

---
+++ README.rst
@@ -0,0 +1,8 @@
+
+::
+
+  $ curl -X POST -H "Content-Type: application/json" -d '{
+        "Log_code": "ADSRTB0001",
+        "Filepath": "adserver/rtb",
+        "Line_no": 10}'
+    localhost:4000/ersolv
--- api/api.go
+++ api/api.go
@@ -1,34 +1,75 @@
 package api
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"encoding/json"
 	"github.com/pm-ashim-ghosh/ersolv/db"
 )
 
 type LogHandler struct{}
 
 func (h LogHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	var errorLog db.CodeLog
-	var err error
-
 	if r.Method == "GET" {
-		errorLog, err = db.GetCodeLog()
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		out := fmt.Sprintf("%v\n", errorLog)
-		if _, err = w.Write([]byte(out)); err != nil {
-			log.Panic(err.Error())
-		}
-
+		showLogs(w, r)
 
 	} else if r.Method == "POST" {
+		createLog(w, r)
 
 	} else {
 		// Unsupported method
+		// TODO: Figure out appropriate JSON reponse.
+	}
+}
+
+func showLogs(w http.ResponseWriter, r *http.Request) {
+	var errorLog db.CodeLog
+	var err error
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	errorLog, err = db.GetCodeLog()
+	if err != nil {
+		log.Panic(err.Error())
+	}
+
+	enc := json.NewEncoder(w)
+	if err = enc.Encode(errorLog); err != nil {
+		log.Panic(err.Error())
+	}
+}
+
+func createLog(w http.ResponseWriter, r *http.Request) {
+	var errorLog db.CodeLog
+	var err error
+	var logId struct{
+		Id int64
+	}
+
+	// Write the POST data to database.
+
+	dec := json.NewDecoder(r.Body)
+	if err = dec.Decode(&errorLog); err != nil {
+		log.Panic(err.Error())
+	}
+
+	// TODO: Validate errorLog
+
+	logId.Id, err = db.CreateCodeLog(errorLog)
+	if err != nil {
+		log.Panic(err.Error())
+	}
+
+
+	// Reply with logId
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	enc := json.NewEncoder(w)
+	if err = enc.Encode(logId); err != nil {
+		log.Panic(err.Error())
 	}
 }
 
